174bce2db50f96f5b8349e2b66f899f9af6f5e56
htribbett123-ctrl <htribbett123@gmail>
2026-04-19 10:05:47 -0700

    Fix ECR login line

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,10 @@
 
 REPO_URI="692828329557.dkr.ecr.us-east-1.amazonaws.com/frontend"
 
+echo "Logging into ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $REPO_URI
+
+
 echo "Pulling latest image..."
 docker pull $REPO_URI:latest
 
